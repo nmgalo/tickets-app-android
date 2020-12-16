8210dc71c00fc52d33f23625da1d4c574aa928b3
Nika <nikolozamgalo@gmail.com>
2020-12-17 00:14:40 +0400

    sample element added for test

--- domain/src/main/java/dev/nmgalo/domain/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package dev.nmgalo.domain
-
-class MyClass {
-}
---
+++ presentation/src/main/java/dev/nmgalo/presentation/utils/view/SampleCustomElement.kt
@@ -0,0 +1,17 @@
+package dev.nmgalo.presentation.utils.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class SampleCustomElement @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+    init {
+
+    }
+
+}
